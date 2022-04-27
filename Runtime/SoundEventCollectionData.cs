4e84316b9ba6e654beff1f19dc39ea6f5232e700
Alistair O'Brien <alistair.m.obrien@gmail.com>
2022-04-27 23:49:20 +0900

    Fix namespace issue

--- Runtime/SoundEventCollectionData.cs
+++ Runtime/SoundEventCollectionData.cs
@@ -1,20 +1,22 @@
-﻿using PixelDust.Audiophile;
-using System;
+﻿using System;
 using UnityEngine;
 using WeightedObjects;
 
-[Serializable]
-public class SoundEventCollectionData
+namespace PixelDust.Audiophile
 {
-    [SerializeField]
-    private WeightedObjectCollection<SoundEvent> soundEventCollection = new WeightedObjectCollection<SoundEvent>();
-
-    [SerializeField]
-    private string soundId;
-    public string SoundId => soundId;
-
-    public void PlayAt(Vector3 position, string overrideId = null)
+    [Serializable]
+    public class SoundEventCollectionData
     {
-        soundEventCollection.GetRandom()?.PlayAt(position, overrideId != null ? overrideId : SoundId);
+        [SerializeField]
+        private WeightedObjectCollection<SoundEvent> soundEventCollection = new WeightedObjectCollection<SoundEvent>();
+
+        [SerializeField]
+        private string soundId;
+        public string SoundId => soundId;
+
+        public void PlayAt(Vector3 position, string overrideId = null)
+        {
+            soundEventCollection.GetRandom()?.PlayAt(position, overrideId != null ? overrideId : SoundId);
+        }
     }
 }
